e83f7bfed90020e11bbd28feccd7f51ddc18dcb6
Luis Enrique Vazquez Escobar <luis.vazquez@irondev.com.mx>
2017-12-05 10:07:57 -0600

    From Request Validator

---
+++ src/app/Requests/CardForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Gozozo\OpenpayServer\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CardForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'holder_name' => 'required|string',
+            'card_number' => 'required|numeric',
+            'cvv2' => 'required|string|min:3|max:4',
+            'expiration_month' => 'required|numeric|min:0|max:12',
+            'expiration_year' => 'required|string|size:2',
+            'device_session_id' => 'required',
+        ];
+    }
+}
