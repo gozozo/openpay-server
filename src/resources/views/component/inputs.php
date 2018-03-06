42c650762ff394de75ba8f678da91a4b52a6bee3
Luis Enrique Vazquez Escobar <luis.vazquez@irondev.com.mx>
2018-03-06 13:04:08 -0600

    Remove device_session_id input is not necessary

--- src/resources/views/component/inputs.php
+++ src/resources/views/component/inputs.php
@@ -1,3 +1,2 @@
-<input type="hidden" name="device_session_id" id="device_session_id">
 <input type="hidden" name="source_id" id="source_id">
 <input type="hidden" name="use_card_points" id="use_card_points" value="false">
