bd2b3b247e547df849ef3680d5ecf2a7471ee050
Luis Enrique Vazquez Escobar <luis.vazquez@irondev.com.mx>
2018-03-18 22:55:16 -0600

    - Bug with production mode

--- src/resources/views/script/openpay.blade.php
+++ src/resources/views/script/openpay.blade.php
@@ -5,7 +5,7 @@
     $(document).ready(function() {
         OpenPay.setId('{{config('openpay.id')}}');
         OpenPay.setApiKey('{{config('openpay.pk')}}');
-        OpenPay.setSandboxMode({{config('openpay.sandbox')}});
+        OpenPay.setSandboxMode({{!config('openpay.production')? 'true' : 'false'}});
         var deviceSessionId = OpenPay.deviceData.setup('{{$form}}', '{{$input}}');
     });
 </script>
