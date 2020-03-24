2aaa7014e58f6cd885bfcdc24099b5cbe8216c1e
Luis Enrique Vazquez Escobar <luis.vazquez@irondev.com.mx>
2020-03-24 15:37:41 -0600

    En Lang

---
+++ src/resources/lang/en/form.php
@@ -0,0 +1,28 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Form Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    */
+
+    'title' => 'Credit or Debit Card',
+    'holder_name' => 'Cardholder Name',
+    'holder_name_placeholder' => 'The way it appears on the card.',
+    'card_number' => 'Card Number',
+    'card_number_placeholder' => 'xxxx xxxx xxxx xxxx',
+    'expiration_month' => 'Month',
+    'expiration_month_placeholder' => '01',
+    'expiration_year' => 'Year',
+    'expiration_year_placeholder' => '20',
+    'cvv2' => 'CVV',
+    'cvv2_placeholder' => '3 o 4 digits',
+    'transactions_openpay' => 'Transactions processed by:',
+    'credit_card' => 'Credit Cards',
+    'debit_card' => 'Debit Cards',
+    'security' => 'Transactions are securely protected by 256 bit encryption.',
+];
